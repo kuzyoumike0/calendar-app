fe4572e04df12e864d775d12c6dcc187cc9add5e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-08 15:32:54 +0900

    Update db.js

--- sever/db.js
+++ sever/db.js
@@ -4,10 +4,10 @@ const { Pool } = require('pg');
 
 // PostgreSQL接続情報（必要に応じて書き換えてください）
 const pool = new Pool({
-  user: 'your_db_user',       // PostgreSQLのユーザー名
+  user: 'PostgreSQL',       // PostgreSQLのユーザー名
   host: 'localhost',          // DBホスト名（例: 'localhost'）
-  database: 'your_db_name',   // 使用するデータベース名
-  password: 'your_db_password', // パスワード
+  database: 'mycalendar',   // 使用するデータベース名
+  password: 'kanri_MilkSugar_0701', // パスワード
   port: 5432,                 // PostgreSQLのポート（通常は5432）
 });
 
@@ -15,3 +15,4 @@ const pool = new Pool({
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
