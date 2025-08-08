6d776226c1baa1b2118dda90b90bc6baeb3792e2
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-08 14:10:56 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // グローバルCSSを使う場合
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// React 18 の新しい root API を使用
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error("Root element not found");
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
