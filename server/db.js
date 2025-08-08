38ea11b2d32247444a1935a2fa477ea82635052f
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-09 06:42:50 +0900

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg');
 
-// 環境変数からDATABASE_URLを取得
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },  // RailwayのPostgresはSSL必須の場合が多い
+  ssl: { rejectUnauthorized: false },  // RailwayのPostgresはSSL必須
 });
 
 module.exports = {
